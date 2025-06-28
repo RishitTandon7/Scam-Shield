c25ae88ae60fe55e4eb8271eac7f49c42399d4e8
RishitTandon7 <rishit.tandon.7@gmail.com>
2025-06-28 20:12:55 +0530

    latest app update

---
+++ scam_shield/lib/screens/scam_chatbot_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScamChatbotScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Scam Chatbot')),
+      body: Center(child: Text('Scam Chatbot Feature Coming Soon')),
+    );
+  }
+}
