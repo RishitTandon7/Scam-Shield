a75edfeeb3e420cbab9fb95f36385ba776fcbf5e
RishitTandon7 <rishit.tandon.7@gmail.com>
2025-06-28 20:12:53 +0530

    latest app update

---
+++ scam_shield/lib/lib/screens/phishing_email_detection_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PhishingEmailDetectionScreen extends StatelessWidget {
+  const PhishingEmailDetectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Phishing Email Detection')),
+      body: Center(child: Text('Phishing Email Detection Feature Coming Soon')),
+    );
+  }
+}
