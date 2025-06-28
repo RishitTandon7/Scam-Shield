f403ee0bdf41eb9d5907226ee27c08e625c74626
RishitTandon7 <rishit.tandon.7@gmail.com>
2025-06-28 20:12:53 +0530

    latest app update

---
+++ scam_shield/lib/screens/fake_profile_detector_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FakeProfileDetectorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Fake Profile Detector')),
+      body: Center(child: Text('Fake Profile Detector Feature Coming Soon')),
+    );
+  }
+}
