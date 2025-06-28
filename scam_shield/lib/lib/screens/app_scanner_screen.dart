2c2e354a9675aee0b4397456dcc64cd9e99550ec
RishitTandon7 <rishit.tandon.7@gmail.com>
2025-06-28 20:12:51 +0530

    latest app update

---
+++ scam_shield/lib/lib/screens/app_scanner_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppScannerScreen extends StatelessWidget {
+  const AppScannerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('App Scanner')),
+      body: Center(child: Text('App Scanner Feature Coming Soon')),
+    );
+  }
+}
