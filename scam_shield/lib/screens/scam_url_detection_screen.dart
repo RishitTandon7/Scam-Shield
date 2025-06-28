c53911ea510aa5ba1e516a9d5f33207fffa5ffd8
RishitTandon7 <rishit.tandon.7@gmail.com>
2025-06-28 20:12:55 +0530

    latest app update

---
+++ scam_shield/lib/screens/scam_url_detection_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScamUrlDetectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Scam URL Detection')),
+      body: Center(child: Text('Scam URL Detection Feature Coming Soon')),
+    );
+  }
+}
