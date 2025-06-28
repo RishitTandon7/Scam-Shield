2e92389f83df85c85f6659e453432bb3482e9c81
RishitTandon7 <rishit.tandon.7@gmail.com>
2025-06-28 20:12:52 +0530

    latest app update

---
+++ scam_shield/lib/lib/screens/financial_fraud_monitor_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FinancialFraudMonitorScreen extends StatelessWidget {
+  const FinancialFraudMonitorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Financial Fraud Monitor')),
+      body: Center(child: Text('Financial Fraud Monitor Feature Coming Soon')),
+    );
+  }
+}
