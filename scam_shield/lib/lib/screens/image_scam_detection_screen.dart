66c7d535606bb6b4f3396334d69bb67854961357
RishitTandon7 <rishit.tandon.7@gmail.com>
2025-06-28 20:12:52 +0530

    latest app update

---
+++ scam_shield/lib/lib/screens/image_scam_detection_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ImageScamDetectionScreen extends StatelessWidget {
+  const ImageScamDetectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Image Scam Detection')),
+      body: Center(child: Text('Image Scam Detection Feature Coming Soon')),
+    );
+  }
+}
