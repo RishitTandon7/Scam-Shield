633cbd6ce9c464168bd1cdf0767dc858fe843520
RishitTandon7 <rishit.tandon.7@gmail.com>
2025-06-28 20:12:53 +0530

    latest app update

---
+++ scam_shield/lib/screens/fake_news_detection_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FakeNewsDetectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Fake News Detection')),
+      body: Center(child: Text('Fake News Detection Feature Coming Soon')),
+    );
+  }
+}
