7dd97bd1a71bf03b94d11f57d6f62ade769b8af1
RishitTandon7 <rishit.tandon.7@gmail.com>
2025-06-28 20:12:54 +0530

    latest app update

---
+++ scam_shield/lib/screens/online_job_scam_analyzer_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnlineJobScamAnalyzerScreen extends StatelessWidget {
+  const OnlineJobScamAnalyzerScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Online Job Scam Analyzer'),
+      ),
+      body: const Center(
+        child: Text('Online Job Scam Analyzer Feature Coming Soon'),
+      ),
+    );
+  }
+}
